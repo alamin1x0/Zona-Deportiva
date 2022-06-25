9876ec46c42cc8ace5c3cd7fc0cd1729dfa41dd5
alamin1x0 <msasakibalamin@gmail.com>
2022-06-25 11:25:42 +0600

    Zona Deportiva

--- app/src/main/java/com/zonadeportivaplus/ninjalapp/ui/CategoriaFragment.java
+++ app/src/main/java/com/zonadeportivaplus/ninjalapp/ui/CategoriaFragment.java
@@ -1,5 +1,7 @@
 package com.zonadeportivaplus.ninjalapp.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,19 +11,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.zonadeportivaplus.ninjalapp.R;
 
-public class CategoriaFragment extends Fragment {
+public class CategoriaFragment extends Fragment implements View.OnClickListener {
 
-    LinearLayout chi,btv,channel24,rtv,news24,independent, jamona;
+    LinearLayout chi, btv, channel24, rtv, news24, independent, jamona;
 
-    private  String url_channel_chi = "https://rhridoy136.shortcm.li/channeli.m3u8";
-    private  String url_channel_btv = "https://rhridoy136.shortcm.li/btvworld.m3u8";
-    private  String url_channel_channel24 = "https://rhridoy136.shortcm.li/channel24.m3u8";
-    private  String url_channel_rtv = "https://rhridoy136.shortcm.li/rtv.m3u8";
-    private  String url_channel_news24 = "https://vidcdn.vidgyor.com/news24-origin/liveabr/news24-origin/live1/playlist.m3u8";
-    private  String url_channel_independent = "https://rhridoy136.shortcm.li/independenttv.m3u8";
-    private  String url_channel_jamona = "https://rhridoy136.shortcm.li/jamunatv.m3u8";
+    private AdView adView;
+
+    private String url_channel_chi = "https://www.youtube.com/watch?v=v139qinZX2E&ab_channel=SOMOYTV";
+    private String url_channel_btv = "https://www.youtube.com/watch?v=vw2m7BEaApk&ab_channel=RtvLive";
+    private String url_channel_channel24 = "https://www.youtube.com/watch?v=9R7G3hf6j1g&ab_channel=ChanneliTv";
+    private String url_channel_rtv = "https://vidcdn.vidgyor.com/news24-origin/liveabr/news24-origin/live1/playlist.m3u8";
+    private String url_channel_news24 = "https://vidcdn.vidgyor.com/news24-origin/liveabr/news24-origin/live1/playlist.m3u8";
+    private String url_channel_independent = "https://vidcdn.vidgyor.com/news24-origin/liveabr/news24-origin/live1/playlist.m3u8";
+    private String url_channel_jamona = "https://www.youtube.com/watch?v=KedHi6fqx5w&ab_channel=JamunaTV";
 
     public CategoriaFragment() {
 
@@ -39,7 +45,42 @@ public class CategoriaFragment extends Fragment {
         rtv = view.findViewById(R.id.rtv);
         news24 = view.findViewById(R.id.news24);
 
+        chi.setOnClickListener(this);
+        jamona.setOnClickListener(this);
+        btv.setOnClickListener(this);
+        channel24.setOnClickListener(this);
+        rtv.setOnClickListener(this);
+        news24.setOnClickListener(this);
+
+        adView = view.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
 
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.chi) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url_channel_chi));
+            startActivity(intent);
+        } else if (v.getId() == R.id.btv) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url_channel_btv));
+            startActivity(intent);
+        } else if (v.getId() == R.id.channel24) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url_channel_channel24));
+            startActivity(intent);
+        } else if (v.getId() == R.id.rtv) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url_channel_rtv));
+            startActivity(intent);
+        } else if (v.getId() == R.id.news24) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url_channel_news24));
+            startActivity(intent);
+        } else if (v.getId() == R.id.jamunatv) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url_channel_jamona));
+            startActivity(intent);
+        }
+
     }
 }
